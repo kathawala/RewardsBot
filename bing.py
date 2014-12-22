46fd2162614f01419e2f74f1d078176dde8dc5d3
Farhan Kathawala <farhank@stanford.edu>
2014-12-22 01:21:00 -0800

    Remove needless comments

--- bing.py
+++ bing.py
@@ -8,9 +8,6 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 # Want to replace the time.sleep calls with webdriverwait, but not yet
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
 import time
 import os.path
 import subprocess
